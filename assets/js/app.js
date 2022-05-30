ee6238c8d6a6762e9fdd93fc431396b0a939e93e
Cloud Custodian <joe.p.terlecki@gmail.com>
2022-05-30 15:35:30 -0400

    learning

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,74 @@
-function getString() {
-  let userString = document.getElementById("userString").value;
-  let palindromeString = reversedString(userString);
-  let palindromeStatus = checkPalindrome(userString, palindromeString);
+const PalindromeController = (() => {
 
-  displayPalindrome(palindromeStatus, palindromeString);
+  return {
+    convertString: function (input) {
+      let newString = [];
+      for (let char = input.length - 1; char >= 0; char --) {
+        newString += input[char];
+      }
+      return newString;
+    },
 
-}
+    evaluatePalindrome: function (oldString, newString) {
+      let palindromeStatus = false;
+      if (oldString === newString) {
+        palindromeStatus = true;
+        return palindromeStatus;
+      } else {
+        return palindromeStatus;
+      }
+    }
+  }
+})();
 
-function reversedString(userString) {
-  let newString = []
-
-  for (let char = userString.length - 1; char >= 0; char--) {
-    newString += userString[char];
+const UIController = (() => {
+  const uiSelectors = {
+    submitButton: '#buttonSubmit',
+    inputString: '#userString',
+    palindromeOutput: '#message'
   }
 
-  return newString;
+  return {
+    getSelectors: function () {
+      return uiSelectors;
+    },
 
-}
+    getStringInput: function () {
+      return document.querySelector(uiSelectors.inputString).value
+    },
 
-function checkPalindrome(userString, reversedString) {
-  let palindromeStatus = true;
-
-  if (userString === reversedString) {
-    return palindromeStatus;
-  } else {
-    palindromeStatus = false;
-    return palindromeStatus;
+    displayPalindrome: function (palindromeString, palindromeStatus) {
+      if (palindromeStatus === true) {
+        document.querySelector(uiSelectors.palindromeOutput).innerHTML = `<p>${palindromeString} IS A PALINDROME</p>`
+      } else {
+        document.querySelector(uiSelectors.palindromeOutput).innerHTML = `<p>${palindromeString} IS NOT A PALINDROME</p>`
+      }
+    }
   }
-}
 
-function displayPalindrome(palindromeStatus, finalString) {
-  if (palindromeStatus == true) {
-    document.getElementById("message").innerHTML = `<p>${finalString} IS A PALINDROME!</p>`;
-  } else {
-    document.getElementById("message").innerHTML = `<p>${finalString} IS NOT A PALINDROME!</p>`;
+})();
+
+const AppController = ((PalindromeController, UIController) => {
+  const loadEventListeners = function () {
+    const uiSelectors = UIController.getSelectors();
+    document.querySelector(uiSelectors.submitButton).addEventListener('click', submitPalindrome);
   }
-}
+
+  const submitPalindrome = function (event) {
+    const stringInput = UIController.getStringInput();
+    const newString = PalindromeController.convertString(stringInput);
+    const palindromeStatus = PalindromeController.evaluatePalindrome(stringInput, newString);
+    UIController.displayPalindrome(newString, palindromeStatus);
+
+    event.preventDefault();
+  }
+
+  return {
+    init: function () {
+      loadEventListeners();
+    }
+  }
+
+})(PalindromeController, UIController);
+
+AppController.init();
